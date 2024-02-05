930f23342b5c074133f8b43f0146da873a8bf155
naston <nathan@planetpaull.com>
2024-02-04 23:03:45 -0600

    smoke test

--- rag_demo/scripts/smoke_test.py
+++ rag_demo/scripts/smoke_test.py
@@ -1,7 +1,7 @@
 # Just runs .complete to make sure the LLM is listening
 from llama_index.llms import Ollama
 
-llm = Ollama(model="mixtral")
+llm = Ollama(model="tinyllama")
 
-response = llm.complete("Who is Laurie Voss?")
+response = llm.complete("Who is Lionel Messi?")
 print(response)
