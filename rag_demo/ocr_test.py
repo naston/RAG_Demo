e4e0f2ef8a4d926f0ea8c58c30c6054fcf9325a4
naston <nathan@planetpaull.com>
2024-02-04 22:06:59 -0600

    ocr testing

--- rag_demo/ocr_test.py
+++ rag_demo/ocr_test.py
@@ -1,4 +1,4 @@
-from rag_demo.scripts import extract_pdf
+from scripts.process_pdf import extract_pdf
 
 if __name__ == '__main__':
     extract_pdf()
--- rag_demo/scripts/process_pdf.py
+++ rag_demo/scripts/process_pdf.py
@@ -8,49 +8,53 @@ def extract_pdf():
     RAW_DATA = './data/01_raw/'
     PROCESSED_DATA = './data/02_processed/'
     file_list = [f for f in os.listdir(path=RAW_DATA) if f.endswith('.pdf') or f.endswith('.PDF')]
-    processed_list = [f for f in os.listdir(path=PROCESSED_DATA) if f.endswith('.pdf') or f.endswith('.PDF')]
+    processed_list = [f[4:] for f in os.listdir(path=PROCESSED_DATA) if f.endswith('.pdf') or f.endswith('.PDF')]
+    
+    print(len(file_list),len(processed_list))
 
-    #file_list = list(set(file_list) - set(processed_list)) # won't work due to OCR prepend tag
-
+    file_list = list(set(file_list) - set(processed_list)) # won't work due to OCR prepend tag
+    print(file_list)
     '''
     main ocr code, which create new pdf file with OCR_ ahead its origin filename, 
     and error messege can be find in error_log
     '''
     error_log = {}
     for file in file_list:
+        break
+        print('OCR on file',file)
+        if file == file_list[3]:
+            continue
         try:
-            _ = ocrmypdf.ocr(file, 'OCR_'+file,output_type='pdf',skip_text=True,deskew=True)
+            _ = ocrmypdf.ocr(RAW_DATA+file, PROCESSED_DATA+'OCR_'+file,output_type='pdf',skip_text=True,deskew=True)
         except Exception as e:
             if hasattr(e,'message'):
                 error_log[file] = e.message
             else:
                 error_log[file] = e
             continue
+
+    print(error_log)
             
     '''
     extract OCRed PDF using PyMuPDF and save into a pandas dataframe
     '''
-    ocr_file_list = [f for f in os.listdir(path=RAW_DATA) if f.startswith('OCR_') ]
+    ocr_file_list = [f for f in os.listdir(path=PROCESSED_DATA) if f.startswith('OCR_') ]
 
     # PDF extraction
     # informations we want to extract
-    extraction_pdfs = {}
+    text = []
 
     for file in ocr_file_list:
-        #save the results
-        pages_df = pages_df = pd.DataFrame(columns=['text'])
         # file reader
-        doc = fitz.open(file)
-        for page_num in range(doc.pageCount):
-            page = doc.loadPage(page_num)
-            pages_df = pages_df.append({'text': page.getText('text')}, ignore_index=True)
-            
-            
-        extraction_pdfs[file] = pages_df  
-    
-    generate_output(extraction_pdfs)
+        doc = fitz.open(PROCESSED_DATA+file)
+        for page_num in range(doc.page_count):
+            page = doc.load_page(page_num)
+            text.append(page.get_text('text'))
+    print(len(text)) 
+    #generate_output(extraction_pdfs)
 
 def generate_output(pdf_dict):
-    for df in pdf_dict:
+    print('gen output...')
+    for df in pdf_dict.values():
         print(df)
         break
